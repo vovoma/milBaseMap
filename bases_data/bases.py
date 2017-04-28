e55a9c18de42d6916665ff96457f736313455905
Troy Do <hoangdov@gmail.com>
2017-04-27 17:01:20 -0700

    Add milBase.json

--- bases_data/bases.py
+++ bases_data/bases.py
@@ -1,4 +1,6 @@
 import ogr #load ogr libraries
+import geojson
+from geojson import Feature, Point, FeatureCollection
 
 # open ogr datasources and print out number of features
 def open_shapefile(file_path):
@@ -14,8 +16,24 @@ def open_shapefile(file_path):
 	# finish function and save result
 	return datasource
 
-def 
-
+def get_base_location(datasource):
+	layer = datasource.GetLayerByIndex(0)
+	base_locations = []
+	layer.ResetReading()
+	for feature in layer:
+		point = feature.GetFieldAsString(3).split(" ")
+		longtitude = float(point[1].split("(")[1])
+		lattitude = float(point[2].split(")")[0])
+		milBase = Feature(geometry=Point((longtitude, lattitude)))
+		base_locations.append(milBase)
+	return base_locations
 
 datasource_BND = open_shapefile("milBase_BND.shp")
 datasource_PT = open_shapefile("milBase_PT.shp")
+
+
+# Base Location
+base_locations_PT = get_base_location(datasource_PT)
+print(base_locations_PT)
+
+
